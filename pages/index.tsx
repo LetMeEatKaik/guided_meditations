1e886b0569c97307c527a80866eaec9973ee4cd0
Kai Kleinbard <57405817+cre8ture@users.noreply.github.com>
2023-04-14 19:04:54 -0400

    new

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,7 +14,8 @@ function MyDaisyUILayout() {
     <div className={`p-4 ${isDark ? 'bg-gray-800' : 'bg-gradient-to-r from-teal-400 to-blue-500'} h-screen`}>
       <div className="max-w-lg mx-auto rounded-lg shadow-lg p-6">
         <h1 className={`text-3xl font-bold ${isDark ? 'text-white' : 'text-gray-900'} mb-4`}>Text to Speech</h1>
-        <Says noSsr />
+        {/* <Says /> // noSsr /> */}
+        <Says />
       </div>
       <div className="flex justify-center mt-4">
         <button className={`btn btn-lg ${isDark ? 'btn-primary' : 'btn-secondary'}`} onClick={toggleDarkMode}>
